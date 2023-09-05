2c4257932b27d9a5b188791df876012aad21c9cb
Minseon Choi <cms20000414@gmail.com>
2023-09-05 20:00:46 +0900

    Modify count.cc

--- count.cc
+++ count.cc
@@ -1,6 +1,7 @@
 #include "count.h"
 
 void count_number() {
-    // lab2 //
-    // do it yourself //
+    for(int i = 1; i <= 10; i++)
+        std::cout << i << std::endl;
+    std::cout << "2019314562" << std::endl;
 }
